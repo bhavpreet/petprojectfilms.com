fe66c6367fd46ace8ce62b3a296bb4f1efa5b054
Bhavpreet Singh <bhavpreet@gmail.com>
2015-09-16 23:17:38 +0530

    email again

--- contact.php
+++ contact.php
@@ -79,8 +79,8 @@ $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
 $cmd = 'mail("'. $address . '","' . $e_subject . '","' . $msg . '","' . $headers . '");';
-echo 'nohup /usr/bin/php -r \'' . $cmd . '\'' > /dev/null 2>&1 &'
-exec('nohup /usr/bin/php -r \'' . $cmd . '\'' > /dev/null 2>&1 &');
+echo 'nohup /usr/bin/php -r "' . $cmd . '"' > /dev/null 2>&1 &'
+#exec('nohup /usr/bin/php -r \'' . $cmd . '\'' > /dev/null 2>&1 &');
 
 	echo "<fieldset>";
 	echo "<div id='success_page'>";
